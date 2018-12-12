b534f12675feb32e8fb2cd7096df0034dec0435c
Ola <aleksandra.podorska@gmail.com>
2018-12-12 09:25:35 +0100

    initialise project

---
+++ src/graphics/Panel.java
@@ -0,0 +1,19 @@
+package graphics;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Panel extends JPanel{
+
+	private int x, y;
+	private Color color = Color.BLUE;
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		
+	}
+}
