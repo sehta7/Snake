0dde609fd1548b95678137a2dcb58e20db0bfb89
Ola <aleksandra.podorska@gmail.com>
2018-12-12 12:37:53 +0100

    add random apple

--- src/graphics/Panel.java
+++ src/graphics/Panel.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -12,12 +13,16 @@ public class Panel extends JPanel implements ActionListener{
 
 	private int xPosition = 345, yPosition = 220;
 	private int xSnake = xPosition, ySnake = yPosition;
+	private int xApple, yApple, diameter = 10;
 	private int width = 10, height = 10;
 	private Color color = Color.BLUE;
 	private Timer timer = new Timer(500, this);
+	private Timer appleTimer = new Timer(5000, this);
+	private int lastPress;
 	
 	public Panel() {
 		timer.start();
+		appleTimer.start();
 	}
 
 	@Override
@@ -27,34 +32,74 @@ public class Panel extends JPanel implements ActionListener{
 		g.setColor(color);
 		g.fillRect(xPosition, yPosition, width, height);
 
-		//g.fillRect(xSnake, ySnake, width, height);
+		g.fillRect(xSnake, ySnake, width, height);
+		
+		g.setColor(Color.RED);
+		g.fillOval(xApple, yApple, diameter, diameter);
 
+	}
+	
+	public void apple() {
+		Random random = new Random();
+		xApple = random.nextInt(800);
+		yApple = random.nextInt(550);
 	}
 
 	public void right() {
+		lastPress = 39;
 		repaint();
 	}
 
 	public void left() {
+		lastPress = 37;
 		repaint();
 	}
 
 	public void up() {
+		lastPress = 40;
 		repaint();
 	}
 
 	public void down() {
+		lastPress = 38;
 		repaint();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == timer){
-			yPosition -= 10;
-			ySnake = yPosition;
-			height += 10;
+			switch (lastPress) {
+			case 37:
+				xPosition -= 10;
+				ySnake = yPosition;
+				height += 10;
+				break;
+			case 38:
+				yPosition -= 10;
+				ySnake = yPosition;
+				height += 10;
+				break;
+			case 39:
+				xPosition -= 10;
+				ySnake = yPosition;
+				height += 10;
+				break;
+			case 40:
+				yPosition += 10;
+				ySnake = yPosition;
+				height += 10;
+				break;
+			default:
+				yPosition -= 10;
+				ySnake = yPosition;
+				height += 10;
+				break;
+			}
 		    repaint();
 	    }
+		if(e.getSource() == appleTimer){
+			apple();
+		}
 		
 	}
 
