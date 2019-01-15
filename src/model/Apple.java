1eb10bb1fbcdf34982289726dd2df4fb792b2427
Ola <aleksandra.podorska@gmail.com>
2019-01-15 11:16:39 +0100

    refactor apple class

--- src/model/Apple.java
+++ src/model/Apple.java
@@ -1,49 +1,56 @@
 package model;
 
+import java.awt.Point;
 import java.util.concurrent.ThreadLocalRandom;
+
+import logic.Logic;
 
 public class Apple {
 
-	private int x;
-	private int y;
+	private Point position;
 	private int diameter;
 	
 	public Apple() {
 		this.diameter = 10;
+		position = new Point();
 		int x = ThreadLocalRandom.current().nextInt(10, 780);
 		if (x % 10 != 0) {
 			x -= x % 10;
-			this.x = x;
+			this.position.x = x;
 		} else {
-			this.x = x;
+			this.position.x = x;
 		}
 		int y = ThreadLocalRandom.current().nextInt(10, 500);
 		if (y % 10 != 0) {
 			y -= y % 10;
-			this.y = y;
+			this.position.y = y;
 		} else {
-			this.y = y;
+			this.position.y = y;
 		}
 	}
 	
 	public int getX() {
-		return x;
+		return position.x;
 	}
 	
 	public void setX(int x) {
-		this.x = x;
+		this.position.x = x;
 	}
 	
 	public int getY() {
-		return y;
+		return position.y;
 	}
 	
 	public void setY(int y) {
-		this.y = y;
+		this.position.y = y;
 	}
 
 	public int getDiameter() {
 		return diameter;
+	}
+	
+	public void newApple() {
+		Logic.newApple(this);
 	}
 	
 }
